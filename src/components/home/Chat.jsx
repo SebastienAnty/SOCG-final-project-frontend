db7f1a60d89f192e1e3db3400043aca51756dc2e
Sebastien-M Anty <bibianty@gmail.com>
2022-03-09 14:18:30 -0500

    cleaning up code

--- src/components/home/Chat.jsx
+++ src/components/home/Chat.jsx
@@ -14,36 +14,9 @@ import { useCollectionData } from "react-firebase-hooks/firestore";
 import "./chat.css";
 import { useRef, useState } from "react";
 
-function stringToColor(string) {
-  let hash = 0;
-  let i;
-
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = "#";
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.substr(-2);
-  }
-
-  return color;
-}
-
-function stringAvatar(name) {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-  };
-}
-
 function ChatMessage(props) {
   const auth = getAuth();
-  const { text, uid, photoURL } = props.message;
+  const { text, uid } = props.message;
 
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
 
